7e5aa550b5c472723d018fbb0570d235c1e64a1f
jimmygoings50 <jimmygoings50@gmail.com>
2014-07-22 05:52:16 -0500

    Create Amazon_Redirect_to_Support_Adam_Carolla_Vs_Patent_Trolls.js
    Redirects Amazon.com to Adam Carolla's Amazon Revenue Link.
    
    Adam Carolla has a revenue-sharing deal with Amazon. 
    
    When you click through Carolla's site to Amazon He gets a percentage of the transaction. 
    
    This is to help him in his fight against the Patent Trolls. Full story [fundanything.com](https://fundanything.com/en/campaigns/patenttroll)

---
+++ Amazon_Redirect_to_Support_Adam_Carolla_Vs_Patent_Trolls.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       Amazon Redirect to Support Adam Carolla Vs Patent Trolls
+// @namespace  http://www.mikevision.com/
+// @version    1.1
+// @description  Redirects Amazon.com to Adam Carolla's Amazon Revenue Link
+// @include http://www.amazon.com/*
+// @include https://www.amazon.com/*
+// @exclude http://www.amazon.com//ref=*
+// @copyright  2014+
+// ==/UserScript==
+
+window.location.replace("http://www.amazon.com//ref=as_sl_pd_tf_lc?&tag=adamcarolla09-20&camp=15345&creative=331665&linkCode=ur1&adid=08J5N62MYPDM106CXGNH&&ref-refURL=http%3A%2F%2Fadamcarolla.com%2Fsponsors%2F" + window.location.pathname);
